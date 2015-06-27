f4ac9601d4c73c58ad263ec07f50299a5d92a23c
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2015-06-27 18:14:04 +0200

    Auto deploy scripts for gh-pages documentation

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Author: Steve Klabnik
+# github.com/steveklabnik/automatically_update_github_pages_with_travis_example
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+cargo doc
+cd target/doc/
+
+git init
+git config user.name "Lukas Kalbertodt"
+git config user.email "lukas.kalbertodt@gmail.com"
+
+git remote add upstream "https://${GH_TOKEN}@github.com/LukasKalbertodt/telegram-bot.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
