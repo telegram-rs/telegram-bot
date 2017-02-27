8d5e9ca4bdee05d02872a7601ab28f01404b8da1
Fedor Gogolev <knsd@knsd.net>
2017-02-27 10:20:34 +0300

    feat: add Response<T> type

--- raw/src/types/response_parameters.rs
+++ raw/src/types/response_parameters.rs
@@ -1,4 +1,45 @@
+use serde::de::{Deserialize, Deserializer, Error};
+
 use types::*;
+
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
+pub enum Response<T> {
+    Success {
+        result: T,
+    },
+    Error {
+        description: String,
+        parameters: Option<ResponseParameters>,
+    },
+}
+
+impl<T: Deserialize> Deserialize for Response<T> {
+    fn deserialize<D>(deserializer: D) -> Result<Response<T>, D::Error> where D: Deserializer {
+        let raw: RawResponse<T> = Deserialize::deserialize(deserializer)?;
+        match (raw.ok, raw.description, raw.result) {
+            (false, Some(description), None) => {
+                Ok(Response::Error {
+                    description: description,
+                    parameters: raw.parameters,
+                })
+            },
+            (true, None, Some(result)) => {
+                Ok(Response::Success {
+                    result: result,
+                })
+            }
+            _ => Err(D::Error::custom("ambiguous response")),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, PartialOrd, Deserialize)]
+pub struct RawResponse<T> {
+    ok: bool,
+    description: Option<String>,
+    result: Option<T>,
+    parameters: Option<ResponseParameters>,
+}
 
 #[derive(Debug, Clone, PartialEq, PartialOrd, Deserialize)]
 pub struct ResponseParameters {
