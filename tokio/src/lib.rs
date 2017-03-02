4c65c9e8a11b4a0dd2ebdf6f5ca75685faefbd38
Fedor Gogolev <knsd@knsd.net>
2017-03-02 07:12:39 +0300

    feat: reexport types from -raw

--- tokio/src/lib.rs
+++ tokio/src/lib.rs
@@ -13,6 +13,19 @@ mod errors;
 pub mod prelude;
 mod stream;
 
+pub use telegram_bot_raw::{Integer, Float, True};
+pub use telegram_bot_raw::{Update, UpdateKind, RawUpdate};
+pub use telegram_bot_raw::{User, Group, Supergroup, Channel, Chat, RawChat};
+pub use telegram_bot_raw::{Message, MessageKind, RawMessage, Forward, ForwardFrom};
+pub use telegram_bot_raw::{MessageEntity, MessageEntityKind, RawMessageEntity};
+pub use telegram_bot_raw::{Audio, Document, PhotoSize, Sticker, Video, Voice};
+pub use telegram_bot_raw::{Contact, Location, Venue};
+
+pub use telegram_bot_raw::{ParseMode, ChatId, ReplyTo};
+pub use telegram_bot_raw::{ReplyMarkup, InlineKeyboardMarkup, ReplyKeyboardMarkup};
+pub use telegram_bot_raw::{ReplyKeyboardRemove, ForceReply};
+pub use telegram_bot_raw::{SendMessage, GetMe};
+
 pub use self::api::{Api, TelegramFuture};
 pub use self::errors::{Error, Result, ErrorKind};
 pub use stream::UpdatesStream;
