e260a246f7ece8cf9f28a9084bdc0d43bc9a69d8
Fedor Gogolev <knsd@knsd.net>
2017-04-15 14:12:26 +0300

    refactor: reexport types from -raw

--- tokio/src/types.rs
+++ tokio/src/types.rs
@@ -6,8 +6,11 @@ pub use telegram_bot_raw::{MessageEntity, MessageEntityKind, RawMessageEntity};
 pub use telegram_bot_raw::{Audio, Document, PhotoSize, Sticker, Video, Voice};
 pub use telegram_bot_raw::{Contact, Location, Venue};
 
-pub use telegram_bot_raw::{ParseMode, ChatId, MessageId};
+pub use telegram_bot_raw::{ParseMode, ChatId, UserId, MessageId};
 pub use telegram_bot_raw::{ReplyMarkup, InlineKeyboardMarkup, ReplyKeyboardMarkup};
 pub use telegram_bot_raw::{ReplyKeyboardRemove, ForceReply};
-pub use telegram_bot_raw::{GetMe, ForwardMessage, SendLocation, SendMessage};
+pub use telegram_bot_raw::{GetMe, GetUpdates, ForwardMessage, SendLocation, SendMessage};
+pub use telegram_bot_raw::{GetChat, LeaveChat};
 pub use telegram_bot_raw::{ChatAction, SendChatAction};
+pub use telegram_bot_raw::{GetChatAdministrators, GetChatMember, GetChatMembersCount};
+pub use telegram_bot_raw::{KickChatMember, UnbanChatMember};
