b133ce79062a2d011f84436ec191cfb894cd3073
Fedor Gogolev <knsd@knsd.net>
2017-04-22 11:16:25 +0300

    docs: document chat types

--- raw/src/types/chat.rs
+++ raw/src/types/chat.rs
@@ -2,36 +2,54 @@ use serde::de::{Deserialize, Deserializer, Error};
 
 use types::*;
 
+/// This object represents a Telegram user or bot.
 #[derive(Debug, Clone, PartialEq, PartialOrd, Deserialize)]
 pub struct User {
+    /// Unique identifier for this user or bot.
     pub id: Integer,
+    /// User‘s or bot’s first name.
     pub first_name: String,
+    /// User‘s or bot’s last name.
     pub last_name: Option<String>,
+    /// User‘s or bot’s username.
     pub username: Option<String>,
 }
 
+/// This object represents a group.
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub struct Group {
+    /// Unique identifier for this chat.
     pub id: Integer,
+    /// Title, for supergroups, channels and group chats.
     pub title: String,
+    /// True if a group has ‘All Members Are Admins’ enabled.
     pub all_members_are_administrators: bool,
 }
 
+/// This object represents a supergroup.
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub struct Supergroup {
+    /// Unique identifier for this chat.
     pub id: Integer,
+    /// Title, for supergroups, channels and group chats.
     pub title: String,
+    /// Username for supergroup.
     pub username: Option<String>,
 }
 
+/// This object represents a channel.
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub struct Channel {
+    /// Unique identifier for this chat.
     pub id: Integer,
+    /// Title, for supergroups, channels and group chats.
     pub title: String,
+    /// Username for channel.
     pub username: Option<String>,
 }
 
 
+/// This object represents a chat.
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub enum Chat {
     Private(User),
@@ -98,14 +116,22 @@ impl Deserialize for Chat {
     }
 }
 
+/// This object represents a chat, directly mapped.
 #[derive(Debug, Clone, PartialEq, PartialOrd, Deserialize)]
 pub struct RawChat {
+    /// Unique identifier for this chat.
     pub id: Integer,
+    /// Type of chat, can be either “private”, “group”, “supergroup” or “channel”
     #[serde(rename="type")]
     pub type_: String,
+    /// Title, for supergroups, channels and group chats
     pub title: Option<String>,
+    /// Username, for private chats, supergroups and channels if available
     pub username: Option<String>,
+    /// First name of the other party in a private chat
     pub first_name: Option<String>,
+    /// Last name of the other party in a private chat
     pub last_name: Option<String>,
+    /// True if a group has ‘All Members Are Admins’ enabled.
     pub all_members_are_administrators: Option<bool>,
 }
