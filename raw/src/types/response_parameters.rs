07865576ffaacdde453077d73ef2038bfc62d506
Fedor Gogolev <knsd@knsd.net>
2017-04-22 12:30:57 +0300

    docs: document response types

--- raw/src/types/response_parameters.rs
+++ raw/src/types/response_parameters.rs
@@ -2,13 +2,19 @@ use serde::de::{Deserialize, Deserializer, Error};
 
 use types::*;
 
+/// All API responses are from this type. Mostly used internal.
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub enum Response<T> {
+    /// Request was successful.
     Success {
+        /// Response result.
         result: T,
     },
+    /// Request was unsuccessful.
     Error {
+        /// Human-readable description of the result.
         description: String,
+        /// Contains information about why a request was unsuccessful.
         parameters: Option<ResponseParameters>,
     },
 }
@@ -33,16 +39,25 @@ impl<T: Deserialize> Deserialize for Response<T> {
     }
 }
 
+/// Directly mapped telegram API response.
 #[derive(Debug, Clone, PartialEq, PartialOrd, Deserialize)]
 pub struct RawResponse<T> {
+    /// If ‘ok’ equals true, the request was successful.
     ok: bool,
+    /// Human-readable description of the result.
     description: Option<String>,
+    /// Result of the query.
     result: Option<T>,
+    /// Information about why a request was unsuccessful.
     parameters: Option<ResponseParameters>,
 }
 
+/// Contains information about why a request was unsuccessful.
 #[derive(Debug, Clone, PartialEq, PartialOrd, Deserialize)]
 pub struct ResponseParameters {
+    /// The group has been migrated to a supergroup with the specified identifier.
     pub migrate_to_chat_id: Option<Integer>,
+    /// In case of exceeding flood control, the number of seconds left to wait
+    /// before the request can be repeated.
     pub retry_after: Option<Integer>,
 }
