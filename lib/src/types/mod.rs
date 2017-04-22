0d5765da6fbc822a8cf4e3d44724e4c9815dec23
Fedor Gogolev <knsd@knsd.net>
2017-04-22 14:04:19 +0300

    refactor: do not export raw types in telegram-bot

--- lib/src/types/mod.rs
+++ lib/src/types/mod.rs
@@ -2,10 +2,10 @@ pub mod requests;
 pub use requests::*;
 
 pub use telegram_bot_raw::{Integer, Float, True, Request};
-pub use telegram_bot_raw::{Update, UpdateKind, RawUpdate};
-pub use telegram_bot_raw::{User, Group, Supergroup, Channel, Chat, RawChat};
-pub use telegram_bot_raw::{Message, MessageKind, RawMessage, Forward, ForwardFrom};
-pub use telegram_bot_raw::{MessageEntity, MessageEntityKind, RawMessageEntity};
+pub use telegram_bot_raw::{Update, UpdateKind};
+pub use telegram_bot_raw::{User, Group, Supergroup, Channel, Chat};
+pub use telegram_bot_raw::{Message, MessageKind, Forward, ForwardFrom};
+pub use telegram_bot_raw::{MessageEntity, MessageEntityKind};
 pub use telegram_bot_raw::{Audio, Document, PhotoSize, Sticker, Video, Voice};
 pub use telegram_bot_raw::{Contact, Location, Venue};
 
