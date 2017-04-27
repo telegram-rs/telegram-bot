7e15a06cb8447e6ad6588dc355c2c79b50cd8b5b
Steven <s.blake.08@aberdeen.ac.uk>
2017-04-27 20:40:59 +0100

    Fixed comments.

--- raw/src/types/update.rs
+++ raw/src/types/update.rs
@@ -75,7 +75,7 @@ pub struct RawUpdate {
     pub channel_post: Option<Message>,
     /// New version of a channel post that is known to the bot and was edited
     pub edited_channel_post: Option<Message>, 
-    /// pub inline_query: Option<InlineQuery>,
-    /// pub chosen_inline_result: Option<ChosenInlineResult>,
-    /// pub callback_query: Option<CallbackQuery>, 
+    // pub inline_query: Option<InlineQuery>,
+    // pub chosen_inline_result: Option<ChosenInlineResult>,
+    // pub callback_query: Option<CallbackQuery>,
 }
