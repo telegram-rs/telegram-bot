72589c4d975f5a64cf5831f7cf4da0951d310903
Fedor Gogolev <knsd@knsd.net>
2017-04-29 11:33:21 +0300

    feat: add GetFile request
    Fixes #26.

--- lib/src/types/requests.rs
+++ lib/src/types/requests.rs
@@ -1,4 +1,4 @@
-pub use telegram_bot_raw::{GetMe, GetUpdates, ForwardMessage, SendLocation, SendMessage};
+pub use telegram_bot_raw::{GetFile, GetMe, GetUpdates, ForwardMessage, SendLocation, SendMessage};
 pub use telegram_bot_raw::{GetChat, LeaveChat};
 pub use telegram_bot_raw::{SendChatAction};
 pub use telegram_bot_raw::{GetChatAdministrators, GetChatMember, GetChatMembersCount};
