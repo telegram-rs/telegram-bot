59f115c22dbca80503109a0d35e91b00cac07088
Steven Blake <s.blake.08@aberdeen.ac.uk>
2017-05-01 13:23:45 +0100

    Added new and setter functions.

--- raw/src/requests/send_contact.rs
+++ raw/src/requests/send_contact.rs
@@ -16,5 +16,56 @@ pub struct SendContact<'c, 'p, 'f, 'l> {
     #[serde(skip_serializing_if = "Option::is_none")]
     reply_to_message_id: Option<Integer>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    reply_markup: Option<ReplyMarkup>,    
+    reply_markup: Option<ReplyMarkup>,
+}
+
+impl<'c, 'p, 'f, 'l> Request for SendContact<'c, 'p, 'f, 'l> {
+    type Response = Message;
+    type RawResponse = Message;
+
+    fn map(raw: Self::RawResponse) -> Self::Response {
+        raw
+    }
+
+    fn name() -> &'static str {
+        "sendContact"
+    }
+}
+
+impl<'c, 'p, 'f, 'l> SendContact<'c, 'p, 'f, 'l> {
+    pub fn new<C, P, F>(chat: C, phone_number: P, first_name: F) -> Self
+        where C: Into<ChatId<'c>>,
+              P: Into<Cow<'p, str>>,
+              F: Into<Cow<'f, str>>
+    {
+        SendContact {
+            chat_id: chat.into(),
+            phone_number: phone_number.into(),
+            first_name: first_name.into(),
+            last_name: None,
+            disable_notification: false,
+            reply_to_message_id: None,
+            reply_markup: None,
+        }
+    }
+
+    pub fn last_name<F>(mut self, last_name: F) -> Self where F: Into<Cow<'l, str>> {
+        self.last_name = Some(last_name.into());
+        self
+    }
+
+    pub fn disable_notification(mut self) -> Self {
+        self.disable_notification = true;
+        self
+    }
+
+    pub fn reply_to<R>(mut self, to: R) -> Self where R: Into<MessageId> {
+        self.reply_to_message_id = Some(to.into().0);
+        self
+    }
+
+    pub fn reply_markup<R>(mut self, reply_markup: R) -> Self where R: Into<ReplyMarkup> {
+        self.reply_markup = Some(reply_markup.into());
+        self
+    }
 }
