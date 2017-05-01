3fc3e1ae53bec8f931ff2bb2ab601c2211ba006d
Steven <s.blake.08@aberdeen.ac.uk>
2017-05-01 21:33:51 +0100

    Add "CanReplySendVenue" to prelude.

--- lib/src/prelude.rs
+++ lib/src/prelude.rs
@@ -1,6 +1,6 @@
 pub use telegram_bot_raw::{CanReplySendLocation, CanSendLocation};
 pub use telegram_bot_raw::{CanReplySendMessage, CanSendMessage};
-pub use telegram_bot_raw::CanSendVenue;
+pub use telegram_bot_raw::{CanReplySendVenue, CanSendVenue};
 pub use telegram_bot_raw::CanForwardMessage;
 pub use telegram_bot_raw::CanSendChatAction;
 pub use telegram_bot_raw::{CanGetChat, CanGetChatAdministrators, CanGetChatMembersCount};
