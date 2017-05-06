08c609d3020a4545387aed5be6ff89528bbe3894
Fedor Gogolev <knsd@knsd.net>
2017-05-06 23:17:38 +0300

    refactor: export id and conversion types

--- lib/src/types/mod.rs
+++ lib/src/types/mod.rs
@@ -4,12 +4,14 @@ pub use requests::*;
 pub use telegram_bot_raw::{Integer, Float, Request};
 pub use telegram_bot_raw::{Update, UpdateKind};
 pub use telegram_bot_raw::{User, Group, Supergroup, Channel, Chat};
-pub use telegram_bot_raw::{Message, MessageKind, Forward, ForwardFrom};
+pub use telegram_bot_raw::{UserId, GroupId, SupergroupId, ChannelId, ChatId, ChatRef};
+pub use telegram_bot_raw::{Message, MessageId, MessageKind, Forward, ForwardFrom};
 pub use telegram_bot_raw::{MessageEntity, MessageEntityKind};
 pub use telegram_bot_raw::{Audio, Document, PhotoSize, Sticker, Video, Voice};
 pub use telegram_bot_raw::{Contact, Location, Venue, File};
+pub use telegram_bot_raw::{ToChatRef, ToUserId, ToMessageId};
 
-pub use telegram_bot_raw::{ParseMode, ChatId, UserId, MessageId};
+pub use telegram_bot_raw::{ParseMode};
 pub use telegram_bot_raw::{ReplyMarkup, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton};
 pub use telegram_bot_raw::{ReplyKeyboardRemove, ForceReply};
 pub use telegram_bot_raw::{ChatAction};
