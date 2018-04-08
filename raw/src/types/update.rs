457fa7cb6a23d7abf4f9b5d9a1fdb22cae3c7a68
Fedor Gogolev <knsd@knsd.net>
2018-04-08 10:31:43 +0300

    fix: handle errrors during updates parsing
    Related with #92

--- raw/src/types/update.rs
+++ raw/src/types/update.rs
@@ -1,4 +1,7 @@
-use serde::de::{Deserialize, Deserializer};
+use std::fmt;
+
+use serde::de::{Deserialize, Deserializer, Visitor, MapAccess, Error};
+use serde_value::Value;
 
 use types::*;
 
@@ -27,55 +30,112 @@ pub enum UpdateKind {
     // ChosenInlineResult(ChosenInlineResult),
     CallbackQuery(CallbackQuery),
     #[doc(hidden)]
-    Unknown(RawUpdate),
+    Error(String),
+    #[doc(hidden)]
+    Unknown,
 }
 
 impl<'de> Deserialize<'de> for Update {
     fn deserialize<D>(deserializer: D) -> Result<Update, D::Error>
         where D: Deserializer<'de>
     {
-        let raw: RawUpdate = Deserialize::deserialize(deserializer)?;
-        macro_rules! maybe_field {
-            ($name:ident, $variant:ident) => {{
-                if let Some(val) = raw.$name {
-                    return Ok(Update {
-                        id: raw.update_id,
-                        kind: UpdateKind::$variant(val),
-                    })
-                }
-            }}
+        #[derive(Deserialize)]
+        #[serde(field_identifier, rename_all = "snake_case")]
+        enum Field {
+            UpdateId,
+            Message,
+            EditedMessage,
+            ChannelPost,
+            EditedChannelPost,
+            CallbackQuery,
         }
 
-        maybe_field!(message, Message);
-        maybe_field!(edited_message, EditedMessage);
-        maybe_field!(channel_post, ChannelPost);
-        maybe_field!(edited_channel_post, EditedChannelPost);
-        // maybe_field!(inline_query, InlineQuery);
-        // maybe_field!(chosen_inline_result, ChosenInlineResult);
-         maybe_field!(callback_query, CallbackQuery);
+        struct UpdateVisitor;
 
-        Ok(Update {
-            id: raw.update_id,
-            kind: UpdateKind::Unknown(raw),
-        })
+        impl<'de> Visitor<'de> for UpdateVisitor {
+            type Value = Update;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("Update")
+            }
+
+            fn visit_map<V>(self, mut map: V) -> Result<Self::Value, V::Error>
+                where V: MapAccess<'de>
+            {
+                let mut update_id = None;
+                let mut error: Option<V::Error> = None;
+
+                macro_rules! match_fields {
+                    ($(($variant:ident, $name:ident));*; ) => {{
+                        $(
+                            let mut $name = None;
+                        )*
+
+                        while let Some(key) = map.next_key()? {
+                            match key {
+                                Field::UpdateId => {
+                                    if update_id.is_some() {
+                                        return Err(Error::duplicate_field("update_id"));
+                                    }
+                                    update_id = Some(map.next_value()?)
+                                },
+                                $(
+                                    Field::$variant => {
+                                        if $name.is_some() {
+                                            return Err(Error::duplicate_field(stringify!($name)));
+                                        }
+                                        let value = map.next_value::<Value>()?;
+                                        match value.deserialize_into() {
+                                            Ok(value) => $name = Some(value),
+                                            Err(err) => error = Some(Error::custom(err)),
+                                        }
+                                    }
+                                )*
+                            }
+                        }
+
+                        let update_id = update_id.ok_or_else(|| Error::missing_field("update_id"))?;
+
+                        if let Some(err) = error {
+                            return Ok(Update {
+                                id: update_id,
+                                kind: UpdateKind::Error(format!("{}", err))
+                            })
+                        }
+
+                        $(
+                            if let Some(val) = $name {
+                                return Ok(Update {
+                                    id: update_id,
+                                    kind: UpdateKind::$variant(val),
+                                })
+                            }
+                        )*
+
+                        return Ok(Update {
+                            id: update_id,
+                            kind: UpdateKind::Unknown,
+                        })
+                    }};
+                }
+
+                match_fields!(
+                    (Message, message);
+                    (EditedMessage, edited_message);
+                    (ChannelPost, channel_post);
+                    (EditedChannelPost, edited_channel_post);
+                    (CallbackQuery, callback_query);
+                )
+            }
+        }
+
+        const FIELDS: &'static [&'static str] = &[
+            "update_id",
+            "message", "edited_message",
+            "channel_post", "edited_channel_post",
+            "callback_query",
+        ];
+
+        deserializer.deserialize_struct("Duration", FIELDS, UpdateVisitor)
     }
-}
-
-/// This object represents an incoming update. Directly mapped.
-#[derive(Debug, Clone, PartialEq, PartialOrd, Deserialize)]
-pub struct RawUpdate {
-    /// The update‘s unique identifier. Update identifiers start from a certain
-    /// positive number and increase sequentially.
-    pub update_id: Integer,
-    /// New incoming message of any kind — text, photo, sticker, etc.
-    pub message: Option<Message>,
-    /// New version of a message that is known to the bot and was edited
-    pub edited_message: Option<Message>,
-    /// New incoming channel post of any kind — text, photo, sticker, etc.
-    pub channel_post: Option<ChannelPost>,
-    /// New version of a channel post that is known to the bot and was edited
-    pub edited_channel_post: Option<ChannelPost>,
-    // pub inline_query: Option<InlineQuery>,
-    // pub chosen_inline_result: Option<ChosenInlineResult>,
-    pub callback_query: Option<CallbackQuery>,
 }
