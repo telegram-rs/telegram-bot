13b510de55c55087e4fa129df68e7029ec382d83
Shou Ya <shouya@users.noreply.github.com>
2018-04-11 20:05:27 +0800

    curl connector

--- lib/src/connector/curl.rs
+++ lib/src/connector/curl.rs
@@ -5,22 +5,22 @@ use std::sync::Arc;
 use std::rc::Rc;
 
 use antidote::Mutex;
-use curl::easy::{Easy, List};
+use curl::easy::{Easy, Form, List};
 use futures::Future;
 use futures::future::result;
 use tokio_core::reactor::Handle;
 use tokio_curl::Session;
 
 use errors::Error;
-use future::{TelegramFuture, NewTelegramFuture};
+use future::{NewTelegramFuture, TelegramFuture};
 
-use telegram_bot_raw::{HttpRequest, HttpResponse, Method, Body};
+use telegram_bot_raw::{Body, HttpRequest, HttpResponse, Method};
 
 use super::_base::Connector;
 
 /// This connector uses `tokio-curl` backend.
 pub struct CurlConnector {
-    inner: Rc<Session>
+    inner: Rc<Session>,
 }
 
 impl fmt::Debug for CurlConnector {
@@ -32,11 +32,15 @@ impl fmt::Debug for CurlConnector {
 impl CurlConnector {
     pub fn new(handle: &Handle) -> Self {
         CurlConnector {
-            inner: Rc::new(Session::new(handle.clone()))
+            inner: Rc::new(Session::new(handle.clone())),
         }
     }
 
-    fn create_request(&self, token: &str, request: HttpRequest) -> Result<(Easy, Arc<Mutex<Vec<u8>>>), Error> {
+    fn create_request(
+        &self,
+        token: &str,
+        request: HttpRequest,
+    ) -> Result<(Easy, Arc<Mutex<Vec<u8>>>), Error> {
         let mut handle = Easy::new();
 
         let url = request.url.url(token);
@@ -56,7 +60,26 @@ impl CurlConnector {
                 headers.append(&format!("Content-Type: application/json"))?;
                 handle.http_headers(headers)?;
             }
-            body => panic!("Unknown body type {:?}", body)
+            Body::Multipart(parts) => {
+                use telegram_bot_raw::MultipartValue;
+                let mut form = Form::new();
+                for (k, v) in parts.into_iter() {
+                    match v {
+                        MultipartValue::Text(text) => {
+                            let mut part = form.part(&k);
+                            part.contents(text.as_bytes());
+                            part.add().unwrap();
+                        }
+                        MultipartValue::File { filename, path } => {
+                            let mut part = form.part(&k);
+                            part.buffer(filename.as_str(), path.into_bytes());
+                            part.add().unwrap();
+                        }
+                    }
+                }
+                handle.httppost(form).unwrap();
+            }
+            body => panic!("Unknown body type {:?}", body),
         }
 
         let result = Arc::new(Mutex::new(Vec::new()));
@@ -85,9 +108,7 @@ impl Connector for CurlConnector {
             let mut guard = result.lock();
             let prev: &mut Vec<u8> = &mut guard;
             ::std::mem::swap(prev, &mut swap);
-            Ok(HttpResponse {
-                body: Some(swap)
-            })
+            Ok(HttpResponse { body: Some(swap) })
         });
 
         TelegramFuture::new(Box::new(future))
