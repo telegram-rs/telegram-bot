ccaf7be898508851c0a01809fa0d38996125c271
Emulator000 <emulator@hotmail.it>
2018-06-25 15:57:43 +0200

    Separated methods for better customization

--- raw/src/requests/restrict_chat_member.rs
+++ raw/src/requests/restrict_chat_member.rs
@@ -27,37 +27,62 @@ impl Request for RestrictChatMember {
 }
 
 impl RestrictChatMember {
-    pub fn new<C, U>(chat: C, user: U, until_date: Option<i32>, can_send_messages: Option<bool>, can_send_media_messages: Option<bool>, can_send_other_messages: Option<bool>, can_add_web_page_previews: Option<bool>) -> Self where C: ToChatRef, U: ToUserId {
+    pub fn new<C, U>(chat: C, user: U) -> Self where C: ToChatRef, U: ToUserId {
         RestrictChatMember {
             chat_id: chat.to_chat_ref(),
             user_id: user.to_user_id(),
-            until_date: until_date,
-            can_send_messages: can_send_messages,
-            can_send_media_messages: can_send_media_messages,
-            can_send_other_messages: can_send_other_messages,
-            can_add_web_page_previews: can_add_web_page_previews,
+            until_date: None,
+            can_send_messages: None,
+            can_send_media_messages: None,
+            can_send_other_messages: None,
+            can_add_web_page_previews: None,
         }
+    }
+
+    pub fn until_date(&mut self, value: i32) -> &mut Self {
+        self.until_date = Some(value);
+        self
+    }
+
+    pub fn can_send_messages(&mut self, value: bool) -> &mut Self {
+        self.can_send_messages = Some(value);
+        self
+    }
+
+    pub fn can_send_media_messages(&mut self, value: bool) -> &mut Self {
+        self.can_send_media_messages = Some(value);
+        self
+    }
+
+    pub fn can_send_other_messages(&mut self, value: bool) -> &mut Self {
+        self.can_send_other_messages = Some(value);
+        self
+    }
+
+    pub fn can_add_web_page_previews(&mut self, value: bool) -> &mut Self {
+        self.can_add_web_page_previews = Some(value);
+        self
     }
 }
 
 /// Restrict a user from a group or a supergroup.
 pub trait CanRestrictChatMemberForChat {
-    fn restrict<O>(&self, other: O, until_date: Option<i32>, can_send_messages: Option<bool>, can_send_media_messages: Option<bool>, can_send_other_messages: Option<bool>, can_add_web_page_previews: Option<bool>) -> RestrictChatMember where O: ToUserId;
+    fn restrict<O>(&self, other: O) -> RestrictChatMember where O: ToUserId;
 }
 
 impl<C> CanRestrictChatMemberForChat for C where C: ToChatRef {
-    fn restrict<O>(&self, other: O, until_date: Option<i32>, can_send_messages: Option<bool>, can_send_media_messages: Option<bool>, can_send_other_messages: Option<bool>, can_add_web_page_previews: Option<bool>) -> RestrictChatMember where O: ToUserId {
-        RestrictChatMember::new(self, other, until_date, can_send_messages, can_send_media_messages, can_send_other_messages, can_add_web_page_previews)
+    fn restrict<O>(&self, other: O) -> RestrictChatMember where O: ToUserId {
+        RestrictChatMember::new(self, other)
     }
 }
 
 /// Restrict a user from a group or a supergroup.
 pub trait CanRestrictChatMemberForUser {
-    fn restrict_from<O>(&self, other: O, until_date: Option<i32>, can_send_messages: Option<bool>, can_send_media_messages: Option<bool>, can_send_other_messages: Option<bool>, can_add_web_page_previews: Option<bool>) -> RestrictChatMember where O: ToChatRef;
+    fn restrict_from<O>(&self, other: O) -> RestrictChatMember where O: ToChatRef;
 }
 
 impl<U> CanRestrictChatMemberForUser for U where U: ToUserId {
-    fn restrict_from<O>(&self, other: O, until_date: Option<i32>, can_send_messages: Option<bool>, can_send_media_messages: Option<bool>, can_send_other_messages: Option<bool>, can_add_web_page_previews: Option<bool>) -> RestrictChatMember where O: ToChatRef {
-        RestrictChatMember::new(other, self, until_date, can_send_messages, can_send_media_messages, can_send_other_messages, can_add_web_page_previews)
+    fn restrict_from<O>(&self, other: O) -> RestrictChatMember where O: ToChatRef {
+        RestrictChatMember::new(other, self)
     }
 }
