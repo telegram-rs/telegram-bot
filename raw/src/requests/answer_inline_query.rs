ffbcee4c00eef3f9ccf3d895ba1396a1b7821c08
Goh Wei Wen <goweiwen@gmail.com>
2019-12-16 16:42:41 +0800

    Add missing fields to AnswerInlineQuery

--- raw/src/requests/answer_inline_query.rs
+++ raw/src/requests/answer_inline_query.rs
@@ -1,3 +1,5 @@
+use std::ops::Not;
+
 use crate::requests::*;
 use crate::types::*;
 
@@ -5,7 +7,16 @@ use crate::types::*;
 pub struct AnswerInlineQuery {
     inline_query_id: InlineQueryId,
     results: Vec<InlineQueryResult>,
-    // TODO: Rest of the fields
+    #[serde(skip_serializing_if = "Option::is_none")]
+    cache_time: Option<Integer>,
+    #[serde(skip_serializing_if = "Not::not")]
+    is_personal: bool,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    next_offset: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    switch_pm_text: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    switch_pm_parameter: Option<String>,
 }
 
 impl Request for AnswerInlineQuery {
@@ -38,10 +49,40 @@ impl AnswerInlineQuery {
         AnswerInlineQuery {
             inline_query_id,
             results,
+            cache_time: None,
+            is_personal: false,
+            next_offset: None,
+            switch_pm_text: None,
+            switch_pm_parameter: None,
         }
     }
 
     pub fn add_inline_result<T: Into<InlineQueryResult>>(&mut self, result: T) {
         self.results.push(result.into());
+    }
+
+    pub fn cache_time(&mut self, cache_time: Integer) -> &mut Self {
+        self.cache_time = Some(cache_time);
+        self
+    }
+
+    pub fn is_personal(&mut self) -> &mut Self {
+        self.is_personal = true;
+        self
+    }
+
+    pub fn next_offset(&mut self, next_offset: String) -> &mut Self {
+        self.next_offset = Some(next_offset);
+        self
+    }
+
+    pub fn switch_pm_text(&mut self, switch_pm_text: String) -> &mut Self {
+        self.switch_pm_text = Some(switch_pm_text);
+        self
+    }
+
+    pub fn switch_pm_parameter(&mut self, switch_pm_parameter: String) -> &mut Self {
+        self.switch_pm_parameter = Some(switch_pm_parameter);
+        self
     }
 }
