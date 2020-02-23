bb6d896e7fbb1d9c53760efdba2e90a12bcbfe35
sorz <me@sorz.org>
2020-02-23 20:56:50 +0800

    feat: add field Sticker.file_unique_id

--- raw/src/types/message.rs
+++ raw/src/types/message.rs
@@ -744,8 +744,11 @@ pub struct Document {
 /// This object represents a sticker.
 #[derive(Debug, Clone, PartialEq, PartialOrd, Deserialize)]
 pub struct Sticker {
-    /// Unique identifier for this file.
+    /// Identifier for this file, which can be used to download or reuse the file.
     pub file_id: String,
+    /// Unique identifier for this file, which is supposed to be the same over time and for different bots.
+    /// Can't be used to download or reuse the file.
+    pub file_unique_id: String,
     /// Sticker width.
     pub width: Integer,
     /// Sticker height.
